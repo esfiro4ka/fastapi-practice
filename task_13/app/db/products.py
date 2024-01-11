1a72ec26e1d8ede1a873fdfce00156b87a42ec8f
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2024-01-11 13:44:09 +0400

    Task 13: change model and add migration

--- task_13/app/db/products.py
+++ task_13/app/db/products.py
@@ -1,17 +1,3 @@
-# from sqlalchemy import Column, Integer, String, Float
-# from sqlalchemy.ext.declarative import declarative_base
-
-# Base = declarative_base()
-
-
-# class Product(Base):
-#     __tablename__ = 'products'
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     title = Column(String, index=True)
-#     price = Column(Float)
-#     count = Column(Integer)
-
 import sqlalchemy
 
 metadata = sqlalchemy.MetaData()
@@ -24,4 +10,5 @@ products_table = sqlalchemy.Table(
     sqlalchemy.Column("title", sqlalchemy.String(100), unique=True, index=True),
     sqlalchemy.Column("price", sqlalchemy.Float),
     sqlalchemy.Column("count", sqlalchemy.Integer),
+    sqlalchemy.Column("description", sqlalchemy.String(250)),
 )
